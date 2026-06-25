19d67a220b7f584351ea5e0bbb4b362b448c60e8
AllRedCat <leonismogabriel@gmail.com>
2026-06-25 12:17:03 -0300

    add cmd/api
    forgot about this

---
+++ cmd/api/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+
+	"github.com/AllRedCat/go-social/internal/auth"
+	"github.com/AllRedCat/go-social/internal/database"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Init db connection
+	db, err := database.DbConnect("data/social.db")
+	if err != nil {
+		log.Fatal("Error on init database: ", err)
+	}
+	defer db.Close()
+
+	// Create initial tables if does not exist
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL,
+		avatar_url TEXT DEFAULT '',
+		created_at DATETIME NOT NULL,
+		deleted_at DATETIME
+	);`
+	if _, err := db.Exec(createTableQuery); err != nil {
+		log.Fatal("Fail on create table on database: ", err)
+	}
+
+	// Dependency injection
+	authRepo := auth.NewRepository(db)
+	authService := auth.NewService(authRepo)
+	authHandler := auth.NewHandler(authService)
+
+	// Router init
+	router := gin.Default()
+
+	// Static file server
+	router.Static("/uploads", "./uploads")
+
+	// Register routes
+	authHandler.RegisterRoutes(router)
+
+	log.Println("Server running on port 8080")
+	if err := router.Run("localhost:8080"); err != nil {
+		log.Fatal("Error on init server: ", err)
+	}
+}
